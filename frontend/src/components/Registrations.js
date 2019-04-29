70ec4a4e878c40cacb1a108716023a19a0837b9b
Peter Larsen <petertretorn@gmail.com>
2019-04-29 15:14:25 +0200

    can create project done

--- frontend/src/components/Registrations.js
+++ frontend/src/components/Registrations.js
@@ -29,7 +29,8 @@ class Registrations extends Component {
         invoice: null,
         dropdownOpen: false,
         modalOpen: false,
-        viewInvoice: false
+        viewInvoice: false,
+        isAddingNewProject: false
     }
     
     componentDidMount() {
@@ -60,7 +61,13 @@ class Registrations extends Component {
         this.setState(state => ({
             viewInvoice: !state.viewInvoice
         }));
-    } 
+    }
+
+    toggleAddProject = () => {
+        this.setState(state => ({
+            isAddingNewProject: !state.isAddingNewProject
+        }));
+    }
     
     onFormInputChange = e => {
         this.setState({ [e.target.name]: e.target.value });
@@ -100,6 +107,27 @@ class Registrations extends Component {
             chosenProject
         }));
     }
+
+    submitProject = async (e) => {
+        e.preventDefault();
+    
+        const newProject = {
+            project: this.state.project,
+            customer: this.state.customer,
+            start: this.state.start,
+            baseRate: this.state.baseRate,
+            vatRate: this.state.vatRate
+        };
+
+        const response = await axios.post('api/projects', newProject);
+        
+        this.setState(state => ({
+            projects: [...state.projects, response.data]
+        }));
+        
+        this.chooseProject(response.data.id);
+        this.toggleAddProject();
+    };
     
     invoiceProject() {
         const projectId = this.state.chosenProject.id;
@@ -119,6 +147,8 @@ class Registrations extends Component {
 
     chooseProject(id) {
         const chosenProject = this.state.projects.find(p => p.id === id);
+        
+        chosenProject.registrations = chosenProject.registrations || [];
         chosenProject.registrations = chosenProject.registrations.sort( (r1, r2) => new Date(r1.date) - new Date(r2.date) );
 
         this.setState(state => ({
@@ -140,38 +170,47 @@ class Registrations extends Component {
         return (
             <Fragment>
                 <Container className="mt-3">
-                    <Dropdown className="mb-3" isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
-                        <DropdownToggle caret>
-                        Choose Project
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            {projects.map( ({projectName, id}) => (
-                                <DropdownItem key={id} onClick={this.chooseProject.bind(this, id)}>
-                                    {projectName}
-                                </DropdownItem>        
-                            ))}
-                        
-                        </DropdownMenu>
-                    </Dropdown>
-                    
+                    <Row>
+                        <Col md="6" sm="12">
+                            <Dropdown className="mb-3 mr-3 float-left" isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+                                <DropdownToggle caret>
+                                Choose Project
+                                </DropdownToggle>
+                                <DropdownMenu>
+                                    {projects.map( ({projectName, id}) => (
+                                        <DropdownItem key={id} onClick={this.chooseProject.bind(this, id)}>
+                                            {projectName}
+                                        </DropdownItem>        
+                                    ))}
+                                
+                                </DropdownMenu>
+                            </Dropdown>
+                            <Button
+                                color='success'
+                                onClick={this.toggleAddProject}
+                                >New Project
+                            </Button>
+                        </Col>
+
+                        <Col sm="12" md="6">
+                        {chosenProject && (
+                            <Card body outline color="primary" className="mb-3">
+                                <CardTitle>{chosenProject.projectName}</CardTitle>
+                                <CardText>
+                                    Started on <Moment format="D MMM YYYY">{chosenProject.start}</Moment> for customer {chosenProject.customer}
+                                </CardText>
+                                <Button 
+                                    color='secondary'
+                                    disabled={!invoiceble}
+                                    onClick={() => this.invoiceProject()}
+                                    >{invoiceButtonText}</Button>
+                            </Card>
+                        )}
+                        </Col>
+                    </Row>
+
                     {chosenProject && (
                         <Fragment>
-                            <Row>
-                                <Col sm="6">
-                                    <Card body outline color="primary" className="mb-3 sm-6">
-                                        <CardTitle>{chosenProject.projectName}</CardTitle>
-                                        <CardText>
-                                            Started on <Moment format="D MMM YYYY">{chosenProject.start}</Moment> for customer {chosenProject.customer}
-                                        </CardText>
-                                        <Button 
-                                            color='success'
-                                            disabled={!invoiceble}
-                                            onClick={() => this.invoiceProject()}
-                                            >{invoiceButtonText}</Button>
-                                    </Card>
-                                </Col>
-                            </Row>
-                            
                             <ListGroup>
                                 <Button
                                     color='dark'
@@ -247,6 +286,66 @@ class Registrations extends Component {
                         </ModalBody>
                     </Modal>
                 )}
+
+                <Modal isOpen={this.state.isAddingNewProject} toggle={this.toggleAddProject}>
+                <ModalHeader toggle={this.toggleAddProject}>Create Project</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={this.submitProject}>
+                        <FormGroup>
+                            <Label for="project">Name</Label>
+                            <Input
+                                type="text"
+                                name="project"
+                                id="project"
+                                onChange={this.onFormInputChange}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="customer">Customer</Label>
+                            <Input
+                                type="text"
+                                name="customer"
+                                id="customer"
+                                onChange={this.onFormInputChange}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="start">Project Start</Label>
+                            <Input
+                                type="date"
+                                name="start"
+                                id="start"
+                                onChange={this.onFormInputChange}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='baseRate'>Hourly Base Rate</Label>
+                            <Input
+                                type='decimal'
+                                name='baseRate'
+                                id='baseRate'
+                                onChange={this.onFormInputChange}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='vatRate'>Vat Rate</Label>
+                            <Input
+                                type='decimal'
+                                name='vatRate'
+                                id='vatRate'
+                                onChange={this.onFormInputChange}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Button 
+                                color='dark'
+                                style={{ marginTop: '2rem' }} block>
+                                Submit
+                            </Button>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                </Modal>
                 
             </Fragment>
         )
